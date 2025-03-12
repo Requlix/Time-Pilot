a7ed1dc2243f30ba921d61d054c7194ba535492d
Ray Ward <25ARW05@stu.cpsk12.org>
2025-03-12 12:05:17 -0500

    aniamtion works for player

--- Time Pilot/Animation.cpp
+++ Time Pilot/Animation.cpp
@@ -14,8 +14,11 @@ void Animation::setTextures(std::string text1, std::string text2, int length)
 {
 	for (int i = 0; i < length; i++)
 	{
-		textures[i][0].loadFromFile(text1 + std::to_string(i) + ".png");
-		textures[i][1].loadFromFile(text2 + std::to_string(i) + ".png");
+		sf::Texture tempText1, tempText2;
+		tempText1.loadFromFile(text1 + std::to_string(i) + ".png");
+		tempText2.loadFromFile(text2 + std::to_string(i) + ".png");
+		textures[0].push_back(tempText1);
+		textures[1].push_back(tempText2);
 	}
 }
 
@@ -26,6 +29,6 @@ sf::Sprite Animation::getSprite()
 
 void Animation::setFrame(int angle)
 {
-	sprite.setTexture(textures[angle][tick%2]);
+	sprite.setTexture(textures[tick % 2][angle]);
 	tick++;
 }
