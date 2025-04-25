46a92030ec34279a8e1d4b34ab486508d8ad5bfb
Ray Ward <25ARW05@stu.cpsk12.org>
2025-04-25 11:28:44 -0500

    fixed bomb right way

--- Time Pilot/Bomb.cpp
+++ Time Pilot/Bomb.cpp
@@ -2,16 +2,17 @@
 
 Bomb::Bomb(sf::Vector2f place)
 {
-	animation.setTextures("airman", "airman", 1);
+	animation.setTextures("bomb", "bomb", 1);
 	animation.setFrame(0);
 	speed = 8.5;
 	animation.setPosition(place);
 	tick = Player::tick;
 	if (place.x > 112*4) 
 	{
-		animation.getSprite().rotate(180);
 		speed = -speed;
 	}
+	else
+		animation.getSprite().rotate(180);
 	
 }
 
